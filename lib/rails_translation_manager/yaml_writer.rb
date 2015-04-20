f69b4ba72cdc1e36499b1fa573c7b9e984cba426
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2015-04-20 09:02:07 +0000

    Extract yaml writing functionality into module.

---
+++ lib/rails_translation_manager/yaml_writer.rb
@@ -0,0 +1,17 @@
+require 'yaml'
+
+module YAMLWriter
+
+  # `to_yaml` outputs an initial '---\n', which is supposed to be a document
+  # separator. We don't want this in the written locale files, so we serialize
+  # to a string, remove the header and any trailing whitehspace, and write to
+  # the file.
+  def write_yaml(filepath, data)
+    File.open(filepath, "w") do |f|
+      yaml = data.to_yaml(separator: "")
+      yaml_without_header = yaml.split("\n").map { |l| l.gsub(/\s+$/, '') }[1..-1].join("\n")
+      f.write(yaml_without_header)
+      f.puts
+    end
+  end
+end
