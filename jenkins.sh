460aedf022366a6cc11b88bf2f1a1930fc92bec2
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2018-08-24 14:55:03 +0100

    Update the Jenkins configuration

--- jenkins.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-set -e
-rm -f Gemfile.lock
-bundle install --path "${HOME}/bundles/${JOB_NAME}"
-export GOVUK_APP_DOMAIN=dev.gov.uk
-bundle exec rake test
-if [[ -n "$PUBLISH_GEM" ]]; then
-  bundle exec rake publish_gem --trace
-fi
