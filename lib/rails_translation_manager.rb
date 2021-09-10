7874f0e94c5fb7a3dd04f84961c6fc368fbcfcc6
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2021-09-10 20:26:03 +0100

    Add locale checker helper
    The methods in this helper will be used for the locale checker classes
    in the following commits.

--- lib/rails_translation_manager.rb
+++ lib/rails_translation_manager.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
 require "rails_translation_manager/version"
 require "rails_translation_manager/railtie" if defined?(Rails)
 require "rails-i18n"
+
+require "rails_translation_manager/locale_checker/locale_checker_helper"
 
 module RailsTranslationManager
   autoload :Exporter, "rails_translation_manager/exporter"
