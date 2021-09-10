c8d1a4454978b331c92543a2db27ab18eb60b504
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2021-09-10 21:40:32 +0100

    Add the main checker class
    LocaleChecker links up the other locale classes and generates a report
    applying all our validation rules. It pulls the locales by searching in
    the `locale_path` directory which is given by the calling app. E.g.
    "...collections/config/locales/*/*.yml". The `available_locales` method
    pulls the locales for the application and matches them against the files
    in the locale path dir.
    
    It will concatenate all locale keys for a langauge into one array to be
    passed to the various classes.
    
    The class outputs an error if the path provided doesn't match any eligible
    YAML files which prevents false positives.
    
    The tests for this class act more like integration tests than 'unit'
    tests as they test the code in the other checker classes, but I think
    that is more useful than simply asserting class calls in this instance.

---
+++ lib/rails_translation_manager/locale_checker.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+module RailsTranslationManager
+  class LocaleChecker
+    attr_reader :locale_path
+
+    CHECKER_CLASSES = [MissingEnglishLocales,
+                       MissingForeignLocales,
+                       IncompatiblePlurals].freeze
+
+    def initialize(locale_path)
+      @locale_path = locale_path
+    end
+
+    def validate_locales
+      output_result
+
+    rescue AllLocales::NoLocaleFilesFound => e
+      puts e
+      false
+    end
+
+    private
+
+    def output_result
+      errors = checker_errors.compact
+
+      if errors.blank?
+        puts "Locale files are in sync, nice job!"
+        true
+      else
+        errors.each do |error_message|
+          puts "\n"
+          puts error_message
+          puts "\n"
+        end
+        false
+      end
+    end
+
+    def checker_errors
+      CHECKER_CLASSES.flat_map do |checker|
+        checker.new(all_locales).report
+      end
+    end
+
+    def all_locales
+      @all_locales ||= AllLocales.new(locale_path).generate
+    end
+  end
+end
