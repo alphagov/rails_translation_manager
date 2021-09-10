9eaeae46f91c46526bd48e6e638bfa687a3bbcee
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2021-09-10 20:32:56 +0100

    Add RSpec spec_helper
    This contains:
    
    a) config we'll need to run the RSpec tests.
    b) requires 'rails_translation_manager' which will house further
    configuration and will be where we define the files that are needed.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "rails_translation_manager"
+
+RSpec.configure do |config|
+  config.before do
+    I18n.available_locales = %i[en cy]
+    config.before { allow($stdout).to receive(:puts) }
+  end
+
+  def capture_stdout(&blk)
+    old = $stdout
+    $stdout = fake = StringIO.new
+    blk.call
+    fake.string
+  ensure
+    $stdout = old
+  end
+end
