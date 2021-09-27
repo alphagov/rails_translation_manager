7dc862fa5352ef80a8a3f5220f0b86a33c74c0a5
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2021-09-27 15:57:04 +0100

    Add PluralForms class
    PluralForms will pull all pluralization rules for the available
    languages and put them into an array. The rules originate from
    `rails-i18n` and the list we created to fill in the gaps at
    `config/locales/plurals.rb`. This can then be used to validate plurals
    against the locales plural form.

---
+++ lib/rails_translation_manager/locale_checker/plural_forms.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class PluralForms
+  def self.all
+    I18n.available_locales.each_with_object({}) do |locale, hsh|
+      begin
+        # fetches plural form (rule) from rails-i18n for locale
+        plural_form = I18n.with_locale(locale) { I18n.t!("i18n.plural.keys") }.sort
+      rescue I18n::MissingTranslationData
+        plural_form = nil
+      end
+
+      hsh[locale.downcase] = plural_form
+    end
+  end
+end
