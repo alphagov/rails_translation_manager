4f8907251158c86577e667d58604e7c70bf84bc6
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2021-10-20 13:12:33 +0100

    Allow nested locales to be imported
    This commit adds the functionality to import nested locales from one
    CSV. We need to do this for Collections but will work for any app with
    nested locales, e.g: `config/locales/fr/some_file.yml`.

--- lib/rails_translation_manager/importer.rb
+++ lib/rails_translation_manager/importer.rb
@@ -5,38 +5,39 @@ require_relative "yaml_writer"
 class RailsTranslationManager::Importer
   include YAMLWriter
 
-  attr_reader :locale, :csv_path, :import_directory
+  attr_reader :locale, :csv_path, :import_directory, :nested
 
-  def initialize(locale:, csv_path:, import_directory:)
+  def initialize(locale:, csv_path:, import_directory:, nested:)
     @locale = locale
     @csv_path = csv_path
     @import_directory = import_directory
+    @nested = nested
   end
 
   def import
     csv = CSV.read(csv_path, headers: true, header_converters: :downcase)
-    data = {}
-    csv.each do |row|
+
+    nested ? import_nested_csv(csv) : import_csv(csv)
+  end
+
+  private
+
+  def import_csv(csv, import_yml_path = File.join(import_directory, "#{locale}.yml"))
+    data = csv.each_with_object({}) do |row, hash|
       key = row["key"]
       key_parts = key.split(".")
       if key_parts.length > 1
-        leaf_node = (data[key_parts.first] ||= {})
+        leaf_node = (hash[key_parts.first] ||= {})
         key_parts[1..-2].each do |part|
           leaf_node = (leaf_node[part] ||= {})
         end
         leaf_node[key_parts.last] = parse_translation(row["translation"])
       else
-        data[key_parts.first] = parse_translation(row["translation"])
+        hash[key_parts.first] = parse_translation(row["translation"])
       end
     end
 
-    write_yaml(import_yml_path, {locale.to_s => data})
-  end
-
-  private
-
-  def import_yml_path
-    File.join(import_directory, "#{locale}.yml")
+    write_yaml(import_yml_path, { locale.to_s => data })
   end
 
   def parse_translation(translation)
@@ -56,5 +57,20 @@ class RailsTranslationManager::Importer
     else
       translation
     end
+  end
+
+  def import_nested_csv(csv)
+    group_csv_by_file(csv).each do |group|
+      language_dir =  import_directory.join(locale)
+
+      Dir.mkdir(language_dir) unless Dir.exists?(language_dir)
+
+      import_yml_path = File.join(import_directory, locale, "#{group[0]}.yml")
+      import_csv(group[1], import_yml_path)
+    end
+  end
+
+  def group_csv_by_file(csv)
+    csv.group_by { |row| row["key"].split(".").first }
   end
 end
